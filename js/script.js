5b5072256e9abe426618016dbd2672153657cad4
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-29 16:59:09 +0200

    name fixed(?)

--- js/script.js
+++ js/script.js
@@ -4,22 +4,25 @@ const yourTicketPlaceholder = document.getElementById("your-ticket");
 const yourDistancePlaceholder = document.getElementById("your-distance");
 const realPricePlaceholder = document.getElementById("real-price");
 //today
+//step0
 const send = document.getElementById("send");
 const yourName = document.getElementById("your-name");
 const yourKms = document.getElementById("kms");
 const tren = document.getElementById("trenitalia");
 const yourAge = document.getElementById("age");
 const greetings = document.getElementById("greetings");
-//
-console.log(yourKms);
+//button listener
 send.addEventListener("click", function () {
+  //ticket calc
   let ticket = yourKms.value * 0.21;
   console.log(ticket);
   let price = ticket;
-
+  //tren to show
   tren.classList.remove("hidden");
   tren.classList.add("show");
   yourDistancePlaceholder.innerText = yourKms.value;
+  greetings.innerText = yourName.value;
+  //ticket calc if
   if (yourAge.value === "minorenne") {
     price = ticket - (ticket / 100) * 20;
   }
@@ -28,10 +31,4 @@ send.addEventListener("click", function () {
   }
   yourTicketPlaceholder.innerText = price.toFixed(2);
   realPricePlaceholder.innerText = ticket.toFixed(2);
-  greetings.innerText = yourName.value;
 });
-
-//steps from 1 to 4
-
-//console.log("il tuo biglietto costa", price.toFixed(2), "euro");
-//bonus
